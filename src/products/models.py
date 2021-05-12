908d13471df80b7157ad21c72d82014d83a1a05b
Isabel-Cristina <jose.sepulveda@smartive.eu>
2021-05-12 20:56:32 +0200

    Create Customers, Products, Profiles and Sales models. Using signal to modify instance

--- src/products/models.py
+++ src/products/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.db.models.base import Model
 
 # Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=120)
+    image = models.ImageField(upload_to='products', default='no_picture.png')
+    price = models.FloatField(help_text='in US dollars $')
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return f"{self.name}-{self.created.strftime('%d/%m/%Y')}"
